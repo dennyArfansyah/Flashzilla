8f498a6ed10fd8f32db99585d9439db6b865cc78
dennyArfansyah <dennyarfansyah@gmail.com>
2023-02-13 23:32:11 +0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Denny Arfansyah on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
